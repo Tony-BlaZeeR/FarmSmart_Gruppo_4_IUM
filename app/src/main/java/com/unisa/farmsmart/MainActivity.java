c530d7a6e91c0eb8b441c14d3ab3bf2e7651721e
Sergio <plettrus99@gmail.com>
2020-06-07 22:08:53 +0200

    Creati gli intent di passaggio activity dalla principale alle secondarie.

--- app/src/main/java/com/unisa/farmsmart/MainActivity.java
+++ app/src/main/java/com/unisa/farmsmart/MainActivity.java
@@ -2,13 +2,38 @@ package com.unisa.farmsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button1;
+    Button button2;
+    Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+    }
+
+    public void clickAgricoltore(View view) {
+        Intent intent = new Intent(this, activity_agricoltore.class);
+        startActivity(intent);
+    }
+
+    public void clickAllevatore(View view) {
+        Intent intent = new Intent(this, activity_allevatore.class);
+        startActivity(intent);
+    }
+
+    public void clickVivaista(View view) {
+        Intent intent = new Intent(this, activity_vivaista.class);
+        startActivity(intent);
     }
 }
