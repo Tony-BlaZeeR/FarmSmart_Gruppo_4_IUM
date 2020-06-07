25528a728ff2ecaddff033e691c1f04cf9c81739
Sergio <plettrus99@gmail.com>
2020-06-07 22:17:53 +0200

    Creati gli intent di passaggio activity dalla principale alle secondarie. Secondo Commit

---
+++ app/src/main/java/com/unisa/farmsmart/activity_allevatore.java
@@ -0,0 +1,13 @@
+package com.unisa.farmsmart;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_allevatore extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_allevatore);
+    }
+}
---
+++ app/src/main/java/com/unisa/farmsmart/activity_vivaista.java
@@ -0,0 +1,13 @@
+package com.unisa.farmsmart;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_vivaista extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vivaista);
+    }
+}
