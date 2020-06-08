4b9589237597381ea8965b3f55953b7cdce73427
Tony-BlaZeeR <antoniobis98@gmail.com>
2020-06-08 15:43:04 +0200

    Aggiunto back button per tornare all'activity main

--- app/src/main/java/com/unisa/farmsmart/MainActivity.java
+++ app/src/main/java/com/unisa/farmsmart/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     Button button2;
     Button button3;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
